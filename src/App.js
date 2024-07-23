d2d9dee82734476fdb85c039630fadf065d3fa39
webapplicationuday <uday.ss048@gmail.com>
2024-07-23 17:33:04 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,9 @@
+// These are the list used in the application. You can move them to any component needed.
+// Replace your code here
+import HistorySearch from './components/HistorySearch'
+
+import './App.css'
+
+const App = () => <HistorySearch />
+
+export default App
